e64bbbc04157eb01fb91f19bdf235e0ca4f6f0e4
jutin0852 <jutindikonu8@gmail.com>
2025-02-20 20:38:55 +0100

    done now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
   const isSummaryButtonVisible = characterCount >= 150;
 
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-4">
+    <div className="flex flex-col  h-[100dvh] bg-gradient-to-br from-gray-50 to-gray-100 p-4">
       {/* Chat Output Messages */}
       <div className="flex-1 overflow-y-auto space-y-4" ref={chatOutputRef}>
         {/* Input Text */}
